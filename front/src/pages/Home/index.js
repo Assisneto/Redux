fb62cc615838435a909228faae2c09787d660958
Assisneto Damasceno <netodamasceno12@gmail.com>
2019-11-10 16:47:49 -0300

    hooks

--- front/src/pages/Home/index.js
+++ front/src/pages/Home/index.js
@@ -1,86 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+
+import api from "../../services/api";
 
 import { ProductList } from './styles';
 
 export default function Home() {
+  const [products, setProducts] = useState([]); 
+
+  useEffect( ()=>{
+    api.get('products').then(data =>{
+      setProducts(data.data)
+      console.log(data.data);
+    });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[]);
+
   return (
     <ProductList>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/40/HZM-1749-240/HZM-1749-240_detalhe1.jpg?ims=280x280" 
-        alt="tenis"
-        />
-        <strong>Tênis very cool</strong>
-        <span>R$ 129,90</span>
+      {
+        products.map(product => 
+          (
+          <li key={product.id} >
+            <img src={product.image} 
+            alt={product.title}
+            />
+        <strong>{product.title}</strong>
+          <span>{product.price}</span>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff"/> 3
-          </div>
-          
-          <span>Adicionar ao Carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/40/HZM-1749-240/HZM-1749-240_detalhe1.jpg?ims=280x280" 
-        alt="tenis"
-        />
-        <strong>Tênis very cool</strong>
-        <span>R$ 129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff"/> 3
-          </div>
-          
-          <span>Adicionar ao Carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/40/HZM-1749-240/HZM-1749-240_detalhe1.jpg?ims=280x280" 
-        alt="tenis"
-        />
-        <strong>Tênis very cool</strong>
-        <span>R$ 129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff"/> 3
-          </div>
-          
-          <span>Adicionar ao Carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/40/HZM-1749-240/HZM-1749-240_detalhe1.jpg?ims=280x280" 
-        alt="tenis"
-        />
-        <strong>Tênis very cool</strong>
-        <span>R$ 129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff"/> 3
-          </div>
-          
-          <span>Adicionar ao Carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/40/HZM-1749-240/HZM-1749-240_detalhe1.jpg?ims=280x280" 
-        alt="tenis"
-        />
-        <strong>Tênis very cool</strong>
-        <span>R$ 129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff"/> 3
-          </div>
-          
-          <span>Adicionar ao Carrinho</span>
-        </button>
-      </li>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff"/> 3
+              </div>
+              
+              <span>Adicionar ao Carrinho</span>
+            </button>
+        </li>
+          ))
+   })
     </ProductList>
   );
 }
