987d1c10f3e42bd5a6587a2c8a71da35109f0bdc
Assisneto Damasceno <netodamasceno12@gmail.com>
2019-11-10 15:49:47 -0300

    Cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,64 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from "react-icons/md";
+import { Container,ProductTable,Total } from './styles';
 
 export default function Cart() {
   return (
-    <div />
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th/>
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th/>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img 
+                src='https://static.netshoes.com.br/produtos/40/HZM-1749-240/HZM-1749-240_detalhe1.jpg?ims=280x280'
+                alt='tenis'
+              />
+            </td>
+            <td>
+              <strong>Tênis muito massa</strong>
+              <span>R$129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type='button'>
+                  <MdRemoveCircleOutline size={20} color="#7159c1"/>
+                </button>
+
+                <input type="number" readOnly value={1}/>
+                
+                <button type='button'>
+                  <MdAddCircleOutline size={20} color="#7159c1"/>
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1"/>
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1920,28</strong>
+        </Total>
+      </footer>
+    </Container>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Switch, Route } from 'react-router-dom';
 
-import Home from './pages/Cart';
-import Cart from './pages/Home';
+import Cart  from './pages/Cart';
+import Home from './pages/Home';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path='/' exact component={Home}/>
-      <Route path='/cart' exact component={Cart}/>
+      <Route path='/cart' component={Cart}/>
     </Switch>
   )
 }
