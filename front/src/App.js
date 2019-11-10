fc2993fb041a5da0715bf015da07c52804cbc013
Assisneto Damasceno <netodamasceno12@gmail.com>
2019-11-10 20:55:42 -0300

    reactotron-redux config

--- front/src/App.js
+++ front/src/App.js
@@ -6,6 +6,8 @@ import GlobalStyle from './styles/global';
 import Header from './components/Header';
 import Routes from './routes';
 
+import './config/ReactotronConfig';
+
 import store from "./store";
 
 function App() {
