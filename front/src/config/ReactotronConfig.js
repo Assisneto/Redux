1b4a5c0583bbd201f037d3449ad051eeec067b74
Assisneto Damasceno <netodamasceno12@gmail.com>
2019-11-11 19:58:18 -0300

    add saga

--- front/src/config/ReactotronConfig.js
+++ front/src/config/ReactotronConfig.js
@@ -1,10 +1,11 @@
 import Reactotron from 'reactotron-react-js';
 import { reactotronRedux } from 'reactotron-redux';
-
+import reactotronReduxSaga from 'reactotron-redux-saga';
 
 if (process.env.NODE_ENV === 'development'){
   const tron = Reactotron.configure()
     .use(reactotronRedux())
+    .use(reactotronReduxSaga())
     .connect();
 
     tron.clear();
