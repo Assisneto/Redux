795ed53f258007e59ce8de5627f7cf45d414ab0f
Assisneto Damasceno <netodamasceno12@gmail.com>
2019-11-11 22:28:27 -0300

    React-Toastify

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
@@ -17,6 +18,7 @@ function App() {
         <Header/>
         <Routes/>
         <GlobalStyle/>
+        <ToastContainer autoClose={3000}/>
       </BrowserRouter>
     </Provider>
   );
--- front/src/styles/global.js
+++ front/src/styles/global.js
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
+import 'react-toastify/dist/ReactToastify.css';
 import background from '../assets/image/background.svg';
 
 export default createGlobalStyle`
